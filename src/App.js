a2a902f3e43e54f4dc73d68ed40ad266036a2b6b
jet.semrick@gmail.com <jet.semrick@gmail.com>
2022-07-15 23:16:05 -0400

    Board Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import "./App.css";
 import Navigation from "./components/Navigation";
+import Board from "./components/Board";
 
 function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
+      <Board></Board>
     </div>
   );
 }
