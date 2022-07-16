6e90f021eaf545f0e5cf7dd5444e06e952e6e02e
jet.semrick@gmail.com <jet.semrick@gmail.com>
2022-07-15 20:26:05 -0400

    Navigation Component

---
+++ src/components/Navigation.js
@@ -0,0 +1,40 @@
+/*
+ * Description: Navigation bar component.
+ * Date: 7-15-2022
+ *
+ */
+
+export default function Navigation() {
+  return (
+    <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
+      <div className="mb-2 sm:mb-0">
+        <a
+          href="/home"
+          className="text-2xl no-underline text-grey-darkest hover:text-blue-dark"
+        >
+          Bailey
+        </a>
+      </div>
+      <div>
+        <a
+          href="/home"
+          className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
+        >
+          Dashboard
+        </a>
+        <a
+          href="/documentation"
+          className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
+        >
+          Documentation
+        </a>
+        <a
+          href="/profile"
+          className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
+        >
+          Profile
+        </a>
+      </div>
+    </nav>
+  );
+}
